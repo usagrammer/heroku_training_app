eee03e622477df8ca73da1a610848420b460c2bf
usagrammer <uny0102@gmail.com>
2021-01-09 17:43:38 +0900

    fixed migration files

--- db/migrate/20210109083813_add_column_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToArticle < ActiveRecord::Migration[6.0]
-  def change
-    add_column :articles, :nickname, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_083813) do
+ActiveRecord::Schema.define(version: 2021_01_09_082322) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
   end
 
 end
